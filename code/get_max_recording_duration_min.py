a88df8eeb66166faff989475e8a0c25661cbe83a
Alessio Buccino <alejoe9187@gmail.com>
2024-10-16 16:12:04 +0200

    duration in minutes (not hours)

--- code/get_max_recording_duration_min.py
+++ code/get_max_recording_duration_min.py
@@ -1,5 +1,5 @@
 """
-Print the maximum recording duration.
+Print the maximum recording duration in minutes.
 """
 from pathlib import Path
 import json
@@ -16,5 +16,7 @@ if __name__ == "__main__":
         with open(json_file, "r") as f:
             job_dict = json.load(f)
         durations.append(job_dict["duration"])
-    max_duration_h = int(np.round(np.max(durations)) / 3600)
-    print(max_duration_h)
+    max_duration_min = int(np.round(np.max(durations) / 60))
+    if max_duration_min < 1:
+        max_duration_min = 1
+    print(max_duration_min)
